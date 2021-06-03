f7eb4de856f86f647331cf8869dc8d3c28594fa2
own1019 <own1019@naver.com>
2021-06-03 18:01:56 +0900

    A-B

---
+++ 입출력과사칙연산/1001.java
@@ -0,0 +1,16 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+class Main1001 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sc.close();
+
+        System.out.println(a-b);
+    }    
+}
