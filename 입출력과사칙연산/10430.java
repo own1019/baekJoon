86f851bb1e2839e545b94c48cb88f1a9a01fa942
own1019 <own1019@naver.com>
2021-06-03 18:03:03 +0900

    나머지

---
+++ 입출력과사칙연산/10430.java
@@ -0,0 +1,20 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+class Main10430 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        sc.close();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println( ((A%C) * (B%C)) % C );
+    }    
+}
