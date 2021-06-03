7f0d8b341aebbf382d1daecadd3d794d9da8edda
own1019 <own1019@naver.com>
2021-06-03 17:36:11 +0900

    두 수 비교하기

---
+++ 1330.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main1330 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        sc.close();
+
+        int a = Integer.parseInt(str.split(" ")[0]);
+        int b = Integer.parseInt(str.split(" ")[1]);
+
+        if(a > b) {
+            System.out.println('>');
+        } else if(a < b) {
+            System.out.println('<');
+        } else {
+            System.out.println("==");
+        }
+
+        
+    }
+}
