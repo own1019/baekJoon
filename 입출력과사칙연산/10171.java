dde302d910d6727bfe4d87768ae439684b600db5
own1019 <own1019@naver.com>
2021-06-03 18:02:44 +0900

    고양이

---
+++ 입출력과사칙연산/10171.java
@@ -0,0 +1,10 @@
+package 입출력과사칙연산;
+
+class Main10171 {
+    public static void main(String args[]) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
