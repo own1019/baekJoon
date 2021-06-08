707bd3b9a50f75b4803a40519620e779adf4a7d8
own1019 <own1019@naver.com>
2021-06-08 13:25:00 +0900

    A+B - 8

---
+++ for문/11022.java
@@ -0,0 +1,28 @@
+package for문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main11022 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for(int i=1; i<=n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int addValue = a + b;
+            bw.write("Case #" + i + ": " + a + " + " + b + " = " + addValue + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
