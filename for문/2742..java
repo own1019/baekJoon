06fd39ca42164781e87dd3c22cff6dba0e1b40a7
own1019 <own1019@naver.com>
2021-06-08 13:13:49 +0900

    기찍 N

---
+++ for문/2742..java
@@ -0,0 +1,22 @@
+package for문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main2742 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int m = n;
+        for(int i=1; i<=n; i++ ) {
+            bw.write(m-- + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
