a6cc4d05bb648ee4f82a0286424c5ff87ba2d768
own1019 <own1019@naver.com>
2021-06-09 16:21:21 +0900

    A+B - 4

--- while문/10951.java
+++ while문/10951.java
@@ -7,27 +7,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-class Main1095 {
+class Main10951 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String s = "";
-        while ((s=br.readLine()) != null) {
+        String s;
+        while ((s = br.readLine()) != null ) {
             StringTokenizer st = new StringTokenizer(s);
-            if(st.hasMoreTokens()) {
-                int n = Integer.parseInt(st.nextToken());
-                int m = Integer.parseInt(st.nextToken());
-                if(n+m == 0) {
-                    break;
-                } else {
-                    bw.append(n+m + "\n");
-                }
-            } else {
-                break;
-            }
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            bw.append(n+m + "\n");
         }
         bw.flush();
         bw.close();
-
+        br.close();
+        
     }
 }
