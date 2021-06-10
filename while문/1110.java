50d37cb0c50b8b32f41613de047b6904fcc4ad17
own1019 <own1019@naver.com>
2021-06-10 13:47:46 +0900

    더하기 사이클

---
+++ while문/1110.java
@@ -0,0 +1,43 @@
+package while문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main1110 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine(); // 입력받은 숫자
+        int n = Integer.parseInt(s); // 10보댜 작은 숫자 체크 및 정수화
+        int i = 1;
+
+        if(n < 10) { // 입력받은 숫자가 10보다 작으면 뒤에 0을 붙임
+            s = 0 + s;
+        }
+
+        while(true) {
+            String divinS1 = s.substring(0, 1); // 첫째자리
+            String divinS2 = s.substring(1, 2); // 둘째자리
+            int addDivinSOri = Integer.parseInt(divinS1) + Integer.parseInt(divinS2);
+            if(addDivinSOri < 10) {
+                s = divinS2 + Integer.toString(addDivinSOri);
+                if(Integer.parseInt(s) == n) {
+                    break;
+                }
+            } else {
+                s = divinS2 + Integer.toString(addDivinSOri).substring(1, 2);
+                if(Integer.parseInt(s) == n) {
+                    break;
+                }
+
+            }
+            i++;
+        }
+        bw.append(Integer.toString(i));
+        bw.flush();
+        bw.close();
+    }
+}
