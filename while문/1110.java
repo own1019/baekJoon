8bfa5de2e07ec1ed83baa8a7a8e235a1ec3ae918
own1019 <own1019@naver.com>
2021-06-10 18:54:09 +0900

    더하기 사이클

--- while문/1110.java
+++ while문/1110.java
@@ -13,28 +13,42 @@ class Main1110 {
         String s = br.readLine(); // 입력받은 숫자
         int n = Integer.parseInt(s); // 10보댜 작은 숫자 체크 및 정수화
         int i = 1;
+        int x,y,z = 0;
 
         if(n < 10) { // 입력받은 숫자가 10보다 작으면 뒤에 0을 붙임
             s = 0 + s;
         }
 
         while(true) {
-            String divinS1 = s.substring(0, 1); // 첫째자리
-            String divinS2 = s.substring(1, 2); // 둘째자리
-            int addDivinSOri = Integer.parseInt(divinS1) + Integer.parseInt(divinS2);
-            if(addDivinSOri < 10) {
-                s = divinS2 + Integer.toString(addDivinSOri);
-                if(Integer.parseInt(s) == n) {
-                    break;
-                }
-            } else {
-                s = divinS2 + Integer.toString(addDivinSOri).substring(1, 2);
-                if(Integer.parseInt(s) == n) {
-                    break;
-                }
-
-            }
             i++;
+            x = n/10;
+            y = n%10;
+            z = ((x+y)%10)*10;
+
+            s = "" + z;
+            n = Integer.parseInt(s);
+            System.out.println(s);
+            if(n == Integer.parseInt(s)) {
+                break;
+            }
+
+            // String divinS1 = s.substring(0, 1); // 첫째자리
+            // String divinS2 = s.substring(1, 2); // 둘째자리
+            // int addDivinSOri = Integer.parseInt(divinS1) + Integer.parseInt(divinS2);
+            // if(addDivinSOri < 10) {
+            //     s = divinS2 + Integer.toString(addDivinSOri);
+            //     if(Integer.parseInt(s) == n) {
+            //         break;
+            //     }
+            // } else {
+            //     s = divinS2 + Integer.toString(addDivinSOri).substring(1, 2);
+            //     if(Integer.parseInt(s) == n) {
+            //         break;
+            //     }
+
+            // }
+            // i++;
+            
         }
         bw.append(Integer.toString(i));
         bw.flush();
